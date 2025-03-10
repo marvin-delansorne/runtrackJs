c4e8a896d09c6b5b6424f0f2c0b52f65e3f69b4b
Your Name <marvin.delansorne@laplateforme.io>
2025-03-10 16:40:42 +0100

    Day with console log

---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+function afficherJourSemaines(){
+    const JourSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"] ;
+        
+    for(let i=0 ; i < JourSemaine.length; i++){
+        console.log(JourSemaine[i]);
+    }
+}
+
+afficherJourSemaines();
