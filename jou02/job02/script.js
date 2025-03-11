d0e8acebadc3815cd95563c9140d9f7044f45d2a
Your Name <marvin.delansorne@laplateforme.io>
2025-03-11 15:36:37 +0100

    job02 showhide

---
+++ jou02/job02/script.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener("click", showhide);
+
+function showhide(){
+    let article = document.getElementById("citation");
+    citation.style.display = (citation.style.display === "none")? "block" : "none";
+}
