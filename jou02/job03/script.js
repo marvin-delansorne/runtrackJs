6d5ff566c1c34a6e804c433dd7905c2aefcc7ca9
Your Name <marvin.delansorne@laplateforme.io>
2025-03-11 15:43:11 +0100

    Add Int with Js

---
+++ jou02/job03/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click",addOne);
+
+function addOne(){
+    let compteur = document.getElementById("compteur");
+    compteur.textContent = parseInt(compteur.textContent) + 1;
+}
+
